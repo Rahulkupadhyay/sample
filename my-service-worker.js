cc8663e21ddc31495aee0d2fd5421028190c1086
kumar3r <rahul.kumar6@amadeus.com>
2020-04-02 14:19:09 +0530

    18

--- my-service-worker.js
+++ my-service-worker.js
@@ -1,27 +1,7 @@
 importScripts('./ngsw-worker.js');
 
-function onClick(event) {
-    // Handle the click event and keep the SW alive until it's handled.
-    event.waitUntil(this.handleClick(event.notification, event.action));
-}
-
-function handleClick(notification, action) {
-    return __awaiter$5(this, void 0, void 0, function* () {
-        notification.close();
-        const options = {};
-        // The filter uses `name in notification` because the properties are on the prototype so
-        // hasOwnProperty does not work here
-        NOTIFICATION_OPTION_NAMES.filter(name => name in notification)
-            .forEach(name => options[name] = notification[name]);
-        yield this.broadcast({
-            type: 'NOTIFICATION_CLICK',
-            data: { action, notification: options },
-        });
-    });
-}
-
-self.addEventListener('notificationclick', (event) => {
-    let that = this;
+this.scope.addEventListener('notificationclick', (event) => {
+    let self = this;
     event.waitUntil(async function () {
         const allClients = await clients.matchAll({
             includeUncontrolled: true
@@ -46,7 +26,8 @@ self.addEventListener('notificationclick', (event) => {
             appClient = await clients.openWindow('/sample/');
         }
 
-        // Message the client:
-        that.onClick(event);
+        appClient.postMessage({
+            msg: event,
+          });
     }());
-})
+});
